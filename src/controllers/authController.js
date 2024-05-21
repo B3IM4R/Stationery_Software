57bfb8f33318d5287ad6fc8f047939a287563109
B3IM4R <beimar.mestizo@gmail.com>
2024-05-20 20:28:15 -0500

    Login Implementation

---
+++ src/controllers/authController.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcrypt');
+const db = require('../config/database');
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Consultar la base de datos para encontrar el usuario por username
+    const sql = 'SELECT * FROM users WHERE username = ?';
+    db.query(sql, [username], async (err, rows) => {
+      if (err) {
+        console.error('Error al Buscar el Usuario: ', err);
+        res.status(500).send('Error al Buscar el Usuario');
+        return;
+      }
+
+      if (rows.length === 0) {
+        res.status(400).send('Usuario No Encontrado');
+        return;
+      }
+
+      const user = rows[0];
+
+      // Comparar la contraseña proporcionada con la almacenada en la base de datos
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        res.status(400).send('Contraseña Incorrecta');
+        return;
+      }
+
+      // Autenticación exitosa
+      res.status(200).send('Inicio de Sesión Exitoso!');
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error al Iniciar Sesión');
+  }
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 router.post('/', userController.createUser);
 router.get('/', userController.getUsers);
+
+router.post('/auth', authController.loginUser);
 
 module.exports = router;
